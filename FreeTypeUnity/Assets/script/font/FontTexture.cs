c8e3f4b5f12c7a9c8a3a5545c132b72b372d69be
billwillman <billwillman@qq.com>
2018-10-15 21:29:29 +0800

    【修改】字体FONT TEXTURE

--- FreeTypeUnity/Assets/script/font/FontTexture.cs
+++ FreeTypeUnity/Assets/script/font/FontTexture.cs
@@ -11,6 +11,7 @@ namespace FreeType
         private Texture2D m_FontTex = null;
         private int m_MaxWidth = 256;
         private int m_MaxHeight = 256;
+        private byte[] m_RawData = null;
 
         public FontTexture(int maxWidth = 256, int maxHeight = 256)
         {
@@ -25,7 +26,11 @@ namespace FreeType
                 if (m_FontTex != null)
                     return;
             }
+            
+            int sz = m_MaxWidth * m_MaxHeight;
 
+            m_RawData = new byte[sz];
+            
             if (m_FontTex != null)
             {
                 m_FontTex.Resize(m_MaxWidth, m_MaxHeight, TextureFormat.Alpha8, false);
@@ -34,6 +39,22 @@ namespace FreeType
             {
                 m_FontTex = new Texture2D(m_MaxWidth, m_MaxHeight, TextureFormat.Alpha8, false, true);
             }
+
+            if (m_Combine != null)
+            {
+                m_Combine.ReBuild(m_MaxWidth, m_MaxHeight);
+            }
+            // 填充数据
+            AllFillRawData();
+
+            ApplyTex();
+        }
+
+        private void AllFillRawData()
+        {
+            if (m_Combine == null)
+                return;
+
         }
 
         private void DestroyFontTex()
@@ -56,6 +77,7 @@ namespace FreeType
 
         public void OnDestroy()
         {
+            m_RawData = null;
             DestroyFontTex();
             DestroyFontRectCombine();
         }
@@ -68,5 +90,12 @@ namespace FreeType
             return m_Combine.FindPicNode(value, font.currentSize, font.sizeType, font.hDpi, font.vDpi);
         }
 
+        private void ApplyTex()
+        {
+            if (m_RawData == null || m_FontTex == null)
+                return;
+            m_FontTex.LoadRawTextureData(m_RawData);
+            m_FontTex.Apply(false, true);
+        }
     }
 }
