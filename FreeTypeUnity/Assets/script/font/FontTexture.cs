a3b686ebdab810b4468b3e526c5e12d463c80c9b
billwillman <billwillman@qq.com>
2018-10-15 19:05:39 +0800

    【增加】创建AlphaTex

--- FreeTypeUnity/Assets/script/font/FontTexture.cs
+++ FreeTypeUnity/Assets/script/font/FontTexture.cs
@@ -9,6 +9,32 @@ namespace FreeType
     {
         private FontRectCombine m_Combine = null;
         private Texture2D m_FontTex = null;
+        private int m_MaxWidth = 256;
+        private int m_MaxHeight = 256;
+
+        public FontTexture(int maxWidth = 256, int maxHeight = 256)
+        {
+            m_MaxWidth = maxWidth;
+            m_MaxHeight = maxHeight;
+        }
+
+        private void CreateFontTex(bool isCheckNull = true)
+        {
+            if (isCheckNull)
+            {
+                if (m_FontTex != null)
+                    return;
+            }
+
+            if (m_FontTex != null)
+            {
+                m_FontTex.Resize(m_MaxWidth, m_MaxHeight, TextureFormat.Alpha8, false);
+            }
+            else
+            {
+                m_FontTex = new Texture2D(m_MaxWidth, m_MaxHeight, TextureFormat.Alpha8, false, true);
+            }
+        }
 
         private void DestroyFontTex()
         {
